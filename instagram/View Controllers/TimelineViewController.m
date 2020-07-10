7294ae775c650565678d3b20fddd2a2cf9b95332
Angel Gutierrez <angelgutierrez@fb.com>
2020-07-10 13:15:32 -0700

    login logout functionality

--- instagram/View Controllers/TimelineViewController.m
+++ instagram/View Controllers/TimelineViewController.m
@@ -8,11 +8,12 @@
 
 #import "TimelineViewController.h"
 #import "LoginViewController.h"
-#import "AppDelegate.h"
+#import "SceneDelegate.h"
 #import "Parse/Parse.h"
 
 @interface TimelineViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSMutableArray *posts;
 @property (nonatomic, strong) UIRefreshControl *refreshControl;
 
 @end
@@ -24,26 +25,41 @@
 
 	// Tableview and Delegate init here
 
-//    self.refreshControl = [[UIRefreshControl alloc] init];
-//	[self.refreshControl addTarget:self action:@selector(fetchTweets) forControlEvents:UIControlEventValueChanged];
-//	[self.tableView insertSubview:self.refreshControl atIndex:0];
+    self.refreshControl = [[UIRefreshControl alloc] init];
+	[self.refreshControl addTarget:self action:@selector(fetchPosts) forControlEvents:UIControlEventValueChanged];
+	[self.tableView insertSubview:self.refreshControl atIndex:0];
+}
+
+- (void)fetchPosts {
+	PFQuery *query = [PFQuery queryWithClassName:@"Post"];
+	query.limit = 20;
+	
+	[query findObjectsInBackgroundWithBlock:^(NSArray *posts, NSError *error) {
+		if (posts != nil) {
+			NSLog(@"😎😎😎 Successfully loaded home timeline");
+			self.posts = (NSMutableArray *) posts;
+			
+			[self.tableView reloadData];
+		} else {
+			NSLog(@"%@", error.localizedDescription);
+		}
+	}];
 }
 
 - (IBAction)didTapLogout:(id)sender {
 	NSLog(@"Logout getting called.");
 	
-	AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+	SceneDelegate *sceneDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
 	
 	UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
 	
 	LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
 	
-	appDelegate.window.rootViewController = loginViewController;
+	sceneDelegate.window.rootViewController = loginViewController;
 	
-	[PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
-		// PFUser.current() will now be nil
-	}];
+	[PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error){}];
 }
+
 
 /*
 #pragma mark - Navigation
